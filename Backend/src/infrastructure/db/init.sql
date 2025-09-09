1021e91a15bcc4e9c612918cb868fe22b1899ad8
Michdev <Michael.devtloz@mail.com>
2025-09-09 13:02:07 -0300

    fix: accept spanish characters

--- Backend/src/infrastructure/db/init.sql
+++ Backend/src/infrastructure/db/init.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS empanadas (
   is_sold_out BOOLEAN DEFAULT FALSE,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO empanadas (name, type, filling, price, is_sold_out) VALUES
 ('Pino', 'Horno', NULL, 2500.00, FALSE),
